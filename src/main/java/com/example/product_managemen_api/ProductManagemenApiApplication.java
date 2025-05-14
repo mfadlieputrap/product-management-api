838122408e72002cf692a991d64a3894e25743d3
mfadlieputrap <mfadlieputrap@gmail.com>
2025-05-14 20:14:27 +0700

    Initialize Project

---
+++ src/main/java/com/example/product_managemen_api/ProductManagemenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_managemen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagemenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagemenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/product_managemen_api/ProductManagemenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_managemen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagemenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
