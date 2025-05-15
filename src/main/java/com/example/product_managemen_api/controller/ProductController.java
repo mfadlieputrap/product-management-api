aacf01d07076873e75d01133cfe9f2160a254e75
mfadlieputrap <mfadlieputrap@gmail.com>
2025-05-15 18:51:34 +0700

    feat(exception): create global exception handler

---
+++ src/main/java/com/example/product_managemen_api/controller/ProductController.java
@@ -0,0 +1,7 @@
+package com.example.product_managemen_api.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProductController {
+}
